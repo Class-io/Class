2db5eaa5878c91574c5a81d569f04000e02790ae
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-07 19:02:17 +0100

    Create config/database.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigValidator } from './common/utils/config-validator';
 import config from './config';
 
 async function bootstrap() {
+  if(!await ConfigValidator.validate()) return;
+  
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.setGlobalPrefix(config.APP.PREFIX);
-  
+
   await app.listen(config.APP.PORT);
 }
 
