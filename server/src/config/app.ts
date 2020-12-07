c38d0fe8853fb1675a23b8f4bb348bc8e411dd24
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-07 18:55:50 +0100

    Create utils/config-validator.ts

--- server/src/config/app.ts
+++ server/src/config/app.ts
@@ -4,6 +4,6 @@ config();
 
 export default {
     MODE: process.env.APP_MODE,
-    PREFIX: process.env.APP_PREFIX,
+    PREFIX: process.env.APP_PREFIX || '/',
     PORT: parseInt(process.env.APP_PORT),
 } as const;
