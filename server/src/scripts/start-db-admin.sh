3e10ff7f63bdcb3ff296f0fe1c3de747714bfdfc
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-07 15:45:20 +0100

    Create scripts/start-db-admin.sh

--- server/src/scripts/start-db-admin.sh
+++ server/src/scripts/start-db-admin.sh
@@ -2,18 +2,16 @@
 
 set -e
 
-CONTAINER="class-database";
+CONTAINER="database-admin";
 GREEN_COLOR='\033[1;32m'
 NO_COLOR='\033[0m'
 
 echo "Removing old container [$CONTAINER] and starting new fresh instance of [$CONTAINER]"
 (docker kill $CONTAINER || :) && \
   (docker rm $CONTAINER || :) && \
-  docker run --name $CONTAINER \
-  -p 27018:27018 \
-  -d mongo:4.0 \
-  mongod --port 27018 --bind_ip_all
-
+  docker run --name $CONTAINER -e HOST=0.0.0.0 \
+  -p 1234:1234 \
+  -d mrvautin/adminmongo
 
 echo "Waiting for [$CONTAINER] to start...";
 SLEEP 5;
