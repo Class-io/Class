94bd19468b68a3a3e3deed087ee209b4b6f301b0
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-08 16:25:58 +0100

    Create pipes/validation.pipe.ts

---
+++ server/src/common/pipes/validation.pipe.ts
@@ -0,0 +1,15 @@
+import { ObjectSchema, ValidationResult } from "joi";
+import { Injectable, PipeTransform } from "@nestjs/common";
+import { InputNotValidException } from "../exceptions/input-not-valid.exception";
+
+@Injectable()
+export class ValidationPipe implements PipeTransform {
+    public constructor(private readonly _schema: ObjectSchema) {}
+
+    public transform(value: unknown): unknown {
+        const result: ValidationResult = this._schema.validate(value);
+        if(result.error) throw new InputNotValidException(result.error.message);
+
+        return value;
+    }
+}
