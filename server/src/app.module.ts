6c3ccee41c9651cd6f168c0e56151991567cc22b
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-08 16:52:55 +0100

    Fix import issues across all the files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
+import { AuthModule } from './authentication/auth.module';
 import config from './config';
+import { UsersModule } from './models/user/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(config.DATABASE.URL, { dbName: config.DATABASE.NAME, useFindAndModify: false }),
+    AuthModule,
+    UsersModule
   ],
-  controllers: [],
-  providers: [],
 })
 
 export class AppModule {}
--- server/src/models/user/users.module.ts
+++ server/src/models/user/users.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose/dist/mongoose.module";
 import { UserSchema } from "../../database/schemas/user.schema";
+import { UsersService } from "./users.service";
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: 'User', schema: [UserSchema]}])],
-    providers: []
+    imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema}])],
+    providers: [UsersService],
+    exports: [UsersService]
 })
 
 export class UsersModule {}
