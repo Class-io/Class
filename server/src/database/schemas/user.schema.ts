f77ec551ee1990905a225d34a7a88ba9524a0dec
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-09 09:43:14 +0100

    Update database/schemas.user.schema.ts to contain more descriptive data

--- server/src/database/schemas/user.schema.ts
+++ server/src/database/schemas/user.schema.ts
@@ -1,9 +1,15 @@
 import { Schema } from 'mongoose';
 
 export const UserSchema = new Schema({
-    email: String,
-    username: String,
-    password: String,
+    email: {
+        type: String
+    },
+    username: {
+        type: String
+    },
+    password: {
+        type: String
+    },
     joinedAt: {
         type: Number,
         default: Date.now()
