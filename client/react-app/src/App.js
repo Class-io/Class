51d33de672e824caed03f22c42c2d6c4f275a09b
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-11 21:15:44 +0100

    Merge client directory

---
+++ client/react-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+// import Login from '../src/components/Login.js';
+import JavaScript from './components/Courses/JavaScript/JavaScript';
+
+function App() {
+  return (
+    <div className="App">
+      <JavaScript />
+      {/* <Login /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ client/react-app/src/components/Courses/JavaScript/JavaScript.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Container from "react-bootstrap/Container";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./JavaScript.css";
+
+export default function JavaScript() {
+  return (
+    <div className="Header">
+      <Navbar collapseOnSelect expand="lg" bg="" variant="dark">
+        <Navbar.Brand href="#home">Class {}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <Container fluid className="Overlay">
+        <Container fluid className="HeaderContent">
+          <h1 class="display-1 text-white">Welcome to JavaScript {}</h1>
+          <h2><u>Learn logic</u>, <em>manipulate</em> <strong>content</strong></h2>
+            <h2 class="lead"><strong>Enroll now!</strong></h2>
+        </Container>
+      </Container>
+    </div>
+  );
+}
