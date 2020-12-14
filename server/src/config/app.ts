1e83f3a847535b686735bbfbf25c58a1752ac040
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-14 08:43:05 +0100

    Add PORT environment variable

--- server/src/config/app.ts
+++ server/src/config/app.ts
@@ -5,5 +5,5 @@ config();
 export default {
     MODE: process.env.APP_MODE,
     PREFIX: process.env.APP_PREFIX || '/',
-    PORT: parseInt(process.env.APP_PORT),
+    PORT: parseInt(process.env.PORT || process.env.APP_PORT),
 } as const;
