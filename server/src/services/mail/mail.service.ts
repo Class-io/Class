a577eea77f0f37f1b836c524204302b68fc72998
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-15 13:16:12 +0100

    Create transporter in services/mail/mail.service.ts

--- server/src/services/mail/mail.service.ts
+++ server/src/services/mail/mail.service.ts
@@ -1,6 +1,36 @@
 import { Injectable } from "@nestjs/common";
+import { Transporter, createTransport } from 'nodemailer';
+import { logger } from '../../common/utils/logger';
+import config from '../../config';
 
 @Injectable()
 export class MailService {
-    
+    private readonly _transporter: Transporter;
+
+    constructor() {
+        this._transporter = this._createTransporter();
+        this._verifyTransporter();
+    }
+
+    private _createTransporter(): Transporter {
+        return createTransport({
+            pool: true,
+            service: 'Gmail',
+            auth: {
+                type: 'OAuth2',
+                user: config.MAIL.USER,
+                refreshToken: config.MAIL.REFRESH_TOKEN,
+                clientId: config.MAIL.CLIENT_ID,
+                clientSecret: config.MAIL.CLIENT_SECRET
+            }
+        });
+    };
+
+    private _verifyTransporter(): void {
+        this._transporter.verify((error) => {
+            if(error) {
+                logger.red(error.message);
+            }
+        });
+    }
 }
