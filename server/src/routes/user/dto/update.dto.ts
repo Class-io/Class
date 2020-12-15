ac5ee3f7ca58727fe095b60ac78749f988898c6f
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-15 15:18:30 +0100

    Add confirmationCode field to database/schemas/user.schema.ts

--- server/src/routes/user/dto/update.dto.ts
+++ server/src/routes/user/dto/update.dto.ts
@@ -1,6 +1,4 @@
 export class UpdateUserDTO implements Readonly<UpdateUserDTO> {
-    username?: string;
-    password?: string;
     confirmationCode?: {
         code: string;
         expiresAt: number;
