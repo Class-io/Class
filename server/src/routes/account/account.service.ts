07f41d2a0a85d3c3183fec94affd39bcfc63e7fc
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-16 10:16:15 +0100

    Refactor confirmEmail method in routes/account/account.service.ts

--- server/src/routes/account/account.service.ts
+++ server/src/routes/account/account.service.ts
@@ -6,6 +6,7 @@ import { InvalidAccountTypeException } from '../../common/exceptions/invalid-acc
 import { InvalidConfirmationCodeException } from '../../common/exceptions/invalid-confirmation-code.exception';
 import { ExpiredConfirmationCodeException } from '../../common/exceptions/expired-confirmation-code.exception';
 import { EmailAlreadyConfirmedException } from '../../common/exceptions/email-already-confirmed.exception';
+import { IUser } from '../user/interfaces/IUser';
 
 @Injectable()
 export class AuthService {
@@ -14,26 +15,50 @@ export class AuthService {
     public async confirmEmail(input: ConfirmEmailRequestDTO): Promise<void> {
         const user = await this._usersSerivce.get({ email: input.email });
 
+        this._throwExceptionWhenUserDoesNotExist(user);
+
+        this._throwExceptionWhenAccountIsFromSocialMedia(user);
+
+        this._throwExceptionWhenEmailIsAlreadyConfirmed(user);
+
+        this._throwExceptionWhenConfirmationCodeIsInvalid(user, input.code);
+
+        this._throwExceptionWhenConfirmationCodeIsExpired(user);
+
+        await this._confirmEmailInDatabase(user);
+    }
+
+    private _throwExceptionWhenUserDoesNotExist(user: IUser | null): void {
         if(!user) {
             throw new UserNotFoundException();
         }
+    }
 
+    private _throwExceptionWhenAccountIsFromSocialMedia(user: IUser): void {
         if(user.isSocialMediaAccount) {
             throw new InvalidAccountTypeException();
         }
+    }
 
+    private _throwExceptionWhenEmailIsAlreadyConfirmed(user: IUser): void {
         if(user.isConfirmed) {
             throw new EmailAlreadyConfirmedException();
         }
+    }
 
-        if(user.confirmationCode.code !== input.code) {
+    private _throwExceptionWhenConfirmationCodeIsInvalid(user: IUser, code: string): void {
+        if(user.confirmationCode.code !== code) {
             throw new InvalidConfirmationCodeException();
         }
+    }
 
+    private _throwExceptionWhenConfirmationCodeIsExpired(user: IUser): void {
         if(Date.now() > user.confirmationCode.expiresAt) {
             throw new ExpiredConfirmationCodeException();
         }
+    }
 
+    private async _confirmEmailInDatabase(user: IUser): Promise<void> {
         await this._usersSerivce.updateById(user.id, { confirmationCode: { code: '', expiresAt: Date.now() }, isConfirmed: true });
     }
 }
