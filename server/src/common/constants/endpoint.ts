4765af1fbd84296fde42be53a0cfbf5b1ca68c3b
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-16 10:21:50 +0100

    Create routes/account/account.controller.ts

--- server/src/common/constants/endpoint.ts
+++ server/src/common/constants/endpoint.ts
@@ -2,5 +2,8 @@ export default {
     Auth: {
         LOGIN: '/auth/login',
         REGISTER: '/auth/register'
+    },
+    Account: {
+        CONFIRM_EMAIL: '/account/confirm-email'
     }
 } as const;
---
+++ server/src/routes/account/account.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller, HttpCode, Post } from "@nestjs/common";
+import { ValidationPipe } from "../../common/pipes/validation.pipe";
+import { Constants } from "../../common/constants";
+import { AccountService } from './account.service';
+import { ConfirmEmailRequestDTO } from './dto/confirm-email.dto';
+
+@Controller('/')
+export class AuthController {
+    constructor(private readonly _accountService: AccountService) {}
+    
+    @Post(Constants.Endpoint.Account.CONFIRM_EMAIL)
+    @HttpCode(200)
+    public login(@Body(new ValidationPipe(LoginValidationSchema)) body: ConfirmEmailRequestDTO): Promise<void> {
+        return this._accountService.confirmEmail(body);
+    }
+}
