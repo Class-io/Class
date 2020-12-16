d8a4f4dafdc949f1001a89df3fd72690989d5713
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-16 07:18:21 +0100

    Create exceptions/invalid-confirmation-code.exception.ts

--- server/src/routes/account/account.service.ts
+++ server/src/routes/account/account.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from "@nestjs/common";
 import { UsersService } from "../user/users.service";
 import { JwtService } from "../../services/jwt/jwt.service";
 import { ConfirmEmailRequestDTO } from './dto/confirm-email.dto';
+import { UserNotFoundException } from '../../common/exceptions/user-not-found-exception';
+import { InvalidAccountTypeException } from '../../common/exceptions/invalid-account-type.exception';
 
 @Injectable()
 export class AuthService {
@@ -9,5 +11,17 @@ export class AuthService {
 
     public async confirmEmail(input: ConfirmEmailRequestDTO): Promise<void> {
         const user = await this._usersSerivce.get({ email: input.email });
+
+        if(!user) {
+            throw new UserNotFoundException();
+        }
+
+        if(user.isSocialMediaAccount) {
+            throw new InvalidAccountTypeException();
+        }
+
+        if(user.confirmationCode.code !== input.code) {
+            
+        }
     }
 }
