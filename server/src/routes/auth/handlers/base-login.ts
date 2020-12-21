dcc50c7328796b121b8091b980fd33d6721010ea
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-21 19:28:01 +0100

    Refactor routes/auth/handlers/login.ts

--- server/src/routes/auth/handlers/base-login.ts
+++ server/src/routes/auth/handlers/base-login.ts
@@ -4,13 +4,10 @@ import { IUser } from '../../user/interfaces/IUser';
 import { UsersService } from '../../user/users.service';
 import { LoginResponseDTO } from '../dto/login.dto';
 import { IAccessTokenPayload } from '../interfaces/IAccessTokenPayload';
-import { AuthTokenPayload, LoginDTO } from '../../../types';
-import AccountType from '../../../common/constants/account-type';
+import { LoginDTO } from '../../../types';
 
 export abstract class BaseLoginHandler {
-    protected abstract readonly _accountType: AccountType;
     protected _user: IUser;
-    protected _payload: AuthTokenPayload;
 
     constructor(protected readonly _input: LoginDTO, protected readonly _usersService: UsersService, protected readonly _jwtService: JwtService) {}
 
--- server/src/routes/auth/handlers/social-login.ts
+++ server/src/routes/auth/handlers/social-login.ts
@@ -1,7 +1,12 @@
 import { BaseLoginHandler } from './base-login';
 import { EmailAlreadyExistsException } from '../../../common/exceptions/email-already-exists.exception';
+import AccountType from '../../../common/constants/account-type';
+import { AuthTokenPayload } from '../../../types';
 
 export abstract class SocialLoginHandler extends BaseLoginHandler {
+    protected abstract readonly _accountType: AccountType;
+    protected _payload: AuthTokenPayload;
+    
     protected async _throwExceptionWhenEmailExistsInDatabase(): Promise<void> {
         this._user = await this._usersService.get({ email: this._payload.email });
         const userHasDifferentAccount = this._user && this._user.accountType !== this._accountType;
