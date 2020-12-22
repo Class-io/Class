20f6c62d44dc1e83e840cde706252fd84e55ef7d
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-22 15:11:21 +0100

    Change private fields to protected in routes/account/handlers/base.ts

--- server/src/routes/account/handlers/base.ts
+++ server/src/routes/account/handlers/base.ts
@@ -9,29 +9,29 @@ import { IUser } from '../../user/interfaces/IUser';
 import { UsersService } from '../../user/users.service';
 
 export class BaseAccountHandler {
-    constructor(private readonly _usersSerivce: UsersService) {}
+    constructor(protected readonly _usersSerivce: UsersService) {}
 
-    private _throwExceptionWhenUserDoesNotExist(user: IUser | null): void {
+    protected _throwExceptionWhenUserDoesNotExist(user: IUser | null): void {
         if(!user) throw new UserNotFoundException();
     }
 
-    private _throwExceptionWhenAccountIsFromSocialMedia(user: IUser): void {
+    protected _throwExceptionWhenAccountIsFromSocialMedia(user: IUser): void {
         if(user.accountType !== Constants.AccountType.REGULAR) throw new InvalidAccountTypeException();
     }
 
-    private _throwExceptionWhenEmailIsAlreadyConfirmed(user: IUser): void {
+    protected _throwExceptionWhenEmailIsAlreadyConfirmed(user: IUser): void {
         if(user.isConfirmed) throw new EmailAlreadyConfirmedException();
     }
 
-    private _throwExceptionWhenEmailIsNotConfirmed(user: IUser): void {
+    protected _throwExceptionWhenEmailIsNotConfirmed(user: IUser): void {
         if(!user.isConfirmed) throw new EmailNotConfirmedException();
     }
 
-    private _throwExceptionWhenConfirmationCodeIsInvalid(user: IUser, code: string): void {
+    protected _throwExceptionWhenConfirmationCodeIsInvalid(user: IUser, code: string): void {
         if(user.confirmationCode.code !== code) throw new InvalidConfirmationCodeException();
     }
 
-    private _throwExceptionWhenConfirmationCodeIsExpired(user: IUser): void {
+    protected _throwExceptionWhenConfirmationCodeIsExpired(user: IUser): void {
         if(Date.now() > user.confirmationCode.expiresAt) throw new ExpiredConfirmationCodeException();
     }
 }
