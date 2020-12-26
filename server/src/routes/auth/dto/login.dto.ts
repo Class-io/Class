e9921fc8a0d3e38c781d977c689b1fbd3a23e40c
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-26 09:59:20 +0100

    Create _setCookie method in routes/auth/handlers/base.handler.ts

--- server/src/routes/auth/dto/login.dto.ts
+++ server/src/routes/auth/dto/login.dto.ts
@@ -2,7 +2,3 @@ export class LoginRequestDTO implements Readonly<LoginRequestDTO> {
     email: string;
     password: string;
 }
-
-export class LoginResponseDTO implements Readonly<LoginResponseDTO> {
-    accessToken: string;
-}
