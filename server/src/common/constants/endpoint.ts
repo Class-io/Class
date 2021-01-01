04323e1ff3999ec13ba233e87e0ee90719acee55
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-01 13:39:54 +0100

    Add logout route to constants/endpoint.ts

--- server/src/common/constants/endpoint.ts
+++ server/src/common/constants/endpoint.ts
@@ -3,7 +3,8 @@ export default {
         REGISTER: '/auth/register',
         LOGIN: '/auth/login',
         LOGIN_GOOGLE: '/auth/login-google',
-        LOGIN_GITHUB: '/auth/login-github'
+        LOGIN_GITHUB: '/auth/login-github',
+        LOGOUT: '/auth/logout'
     },
     Account: {
         CHANGE_PASSWORD: '/account/change-password',
