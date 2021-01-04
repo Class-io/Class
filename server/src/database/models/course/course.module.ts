9f923013b07a1738792995a288d88582c4699da5
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-04 09:51:31 +0100

    Create database/models/course/course.repository.ts

--- server/src/database/models/course/course.module.ts
+++ server/src/database/models/course/course.module.ts
@@ -1,12 +1,12 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose/dist/mongoose.module";
 import { CourseSchema } from '../../schemas/course.schema';
-import { CoursesService } from './course.repository';
+import { CourseRepository } from './course.repository';
 
 @Module({
     imports: [MongooseModule.forFeature([{ name: 'Course', schema: CourseSchema }])],
-    providers: [CoursesService],
-    exports: [CoursesService]
+    providers: [CourseRepository],
+    exports: [CourseRepository]
 })
 
-export class CoursesModule {}
+export class CourseModule {}
