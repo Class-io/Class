edd4896b2f3a13ab6f0a8bba90ce41b6dd548317
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-04 16:05:01 +0100

    Create configureCloudinary method in utils/middleware-initiator.ts

--- server/src/common/utils/middleware-initiator.ts
+++ server/src/common/utils/middleware-initiator.ts
@@ -1,9 +1,11 @@
 import { INestApplication } from "@nestjs/common";
 import { ExceptionMiddleware } from "../middlewares/exception.middleware";
 import * as cookieParser from 'cookie-parser';
+import * as cloudinary from 'cloudinary';
 import { serve, setup } from 'swagger-ui-express';
 import { load } from 'yamljs';
 import { join } from 'path';
+import config from '../../config';
 
 export class MiddlewareInitiator {
     public static initiate(app: INestApplication): void {
@@ -12,10 +14,20 @@ export class MiddlewareInitiator {
         app.use(cookieParser());
 
         this.createDocumentation(app);
+
+        this.configureCloudinary();
     };
 
     private static createDocumentation(app: INestApplication): void {
         const swaggerDocs = load(join(__dirname, '/../../../swagger.yml'));
         app.use('/api-docs', serve, setup(swaggerDocs));
+    }
+
+    private static configureCloudinary(): void {
+        cloudinary.v2.config({
+            cloud_name: config.CLOUDINARY.CLOUD_NAME,
+            api_key: config.CLOUDINARY.API_KEY,
+            api_secret: config.CLOUDINARY.API_SECRET
+        });
     }
 }
