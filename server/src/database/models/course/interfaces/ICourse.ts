992df151a5ddc868b437afc66b03bdd221886280
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-07 10:28:08 +0100

    Fix issue with constant exports across all the files

--- server/src/database/models/course/interfaces/ICourse.ts
+++ server/src/database/models/course/interfaces/ICourse.ts
@@ -1,7 +1,7 @@
 import { Document } from 'mongoose';
-import CourseLevel from '../../../../common/constants/course-level';
-import CourseRating from '../../../../common/constants/course-rating';
-import CourseTopic from '../../../../common/constants/course-topic';
+import { CourseLevel } from '../../../../common/constants/course-level';
+import { CourseRating } from '../../../../common/constants/course-rating';
+import { CourseTopic } from '../../../../common/constants/course-topic';
 
 export interface ICourse extends Document {
     readonly id: string;
